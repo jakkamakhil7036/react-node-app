f336350716143f841f38691a867a8ea187f1d9bc
jakkamakhil7036 <akhiljakkam12@gmail.com>
2025-06-02 14:39:35 +0530

    update

--- index.js
+++ index.js
@@ -15,3 +15,12 @@ app.gcet("/name",(req,res)=>{
 app.gcet("/weather",(req,res)=>{
     return res.send("akhil ");
 });
+
+app.get("",(req,res)=>{
+    const products=[
+        {name:"product 1",price:34},
+        {name:"product 2",price:35},
+        {name:"product 3",price:36},
+        
+    ]res.json(products)
+})
