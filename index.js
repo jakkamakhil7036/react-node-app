2e52ce6efd9664faa0ee7fa21b5f774f768ba7d5
jakkamakhil7036 <akhiljakkam12@gmail.com>
2025-06-04 10:08:41 +0530

    update

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ import cors from "cors";
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
+import dotenv from 'dotenv'
+  
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+const MONGODB_URI=process.env.MONGODB_URI
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/orders",orderRouter)
