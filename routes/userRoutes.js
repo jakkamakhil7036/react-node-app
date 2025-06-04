884560311f7de88898bfb2b8e6116c207c33a977
jakkamakhil7036 <akhiljakkam12@gmail.com>
2025-06-04 14:13:52 +0530

    modified userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,11 @@ import userModel from "../models/userModel.js";
 
 const userRouter = express.Router();
 
+// Test route
+userRouter.get("/", (req, res) => {
+  res.send("Users route is working!");
+});
+
 userRouter.post("/register", async (req, res) => {
   const { name, email, pass } = req.body;
   const result = await userModel.insertOne({
@@ -28,7 +33,7 @@ userRouter.get("/:id", async (req, res) => {
 
 userRouter.get("/:id/name", async (req, res) => {
   const email = req.params.id;
-  const result = await userModel.findOne({ email },{_id:0,name:1});
+  const result = await userModel.findOne({ email }, { _id: 0, name: 1 });
   return res.json(result);
 });
 
