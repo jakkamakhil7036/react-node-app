01d3135019c7790840a49192565e43538b01b1db
jakkamakhil7036 <akhiljakkam12@gmail.com>
2025-06-05 09:40:31 +0530

    update

--- index.js
+++ index.js
@@ -1,30 +1,34 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
+import dotenv from 'dotenv';
 import userRouter from "./routes/userRoutes.js";
 import productRouter from "./routes/productRoutes.js";
 import orderRouter from "./routes/orderRoutes.js";
-import dotenv from 'dotenv';
+
 dotenv.config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-const MONGODB_URI = process.env.MONGODB_URI
+const DBUSER = encodeURIComponent(process.env.DBUSER)
+const DBPASS = encodeURIComponent(process.env.DBPASS)
+const MONGODB_URI =`mongodb+srv://${DBUSER}:${DBPASS}@cluster0.qjxhv.mongodb.net/gcet?retryWrites=true&w=majority&appName=Cluste`
 
+// const MONGO_URI = process.env.MONGO_URI
+//testing
 app.use("/users", userRouter);
 app.use("/products", productRouter);
 app.use("/orders",orderRouter)
 
 mongoose
-.connect(MONGODB_URI)
-.then(()=>{
-  app.listen(8080,()=>{
-    console.log("Server Started on port 8080");
+  .connect(MONGO_URI)
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server Started on port 8080");
+    });
   })
-})
-.catch((error)=>{
-  console.log(error)
-})
+  .catch((error) => {
+    console.log(error);
+  });
